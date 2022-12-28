27a903200096e92ff4423cf8988c91c88e34796b
Mahnoor <mariyammahnoor@gmail.com>
2022-12-29 01:44:26 +0530

    setup for states

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,123 +11,126 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Contact App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.orange,
       ),
-      home: const MyHome(),
+      home: MyHome(),
+      routes: {
+        '/new_contact':(context)=> const NewContactViewState(),
+      },
     );
   }
 }
 
+class Contact {
+  final String name;
+
+  const Contact({required this.name});
+}
+
+class ContactBook {
+  //make a singleton class
+  ContactBook._sharedInstance();
+
+  static final ContactBook _shared = ContactBook._sharedInstance();
+
+  factory ContactBook() => _shared;
+
+  // storage for the contact
+  final List<Contact> _contact = [
+    Contact(name: 'Mahnoor'),
+    Contact(name: 'Hamza'),
+    Contact(name: 'Owais')
+  ];
+
+// get length of the contact
+  int get length => _contact.length;
+
+  void add({required Contact contact}) {
+    _contact.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contact.remove(contact);
+  }
+
+  Contact? contact({required int atIndex}) =>
+      _contact.length > atIndex ? _contact[atIndex] : null;
+}
+
 class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
+  MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Book'),
+      ),
+      body: ListView.builder(
+          itemCount: contactBook.length,
+          itemBuilder: (context, int index) {
+            final contact = contactBook.contact(atIndex: index)!;
+            return ListTile(
+              title: Text(contact.name),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new_contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewContactViewState extends StatefulWidget {
+  const NewContactViewState({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactViewState> createState() => _NewContactViewStateState();
+}
+
+class _NewContactViewStateState extends State<NewContactViewState> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hey Mahnoor'),
+        title: const Text('Add New Contact'),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.topRight,
-              child: const Icon(
-                Icons.search,
-              ),
-            ),
-            Container(
-              height: 200,
-              width: 200,
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Colors.black),
-              child: Image.network(
-                'https://images.pexels.com/photos/14771128/pexels-photo-14771128.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                fit: BoxFit.cover,
-                color: Colors.black.withOpacity(0.3),
-                colorBlendMode: BlendMode.color,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'PIANO',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
-                        width: 8),
-                    borderRadius: BorderRadius.circular(4.0)),
-                child: Row(
-                  children: [
-                    buildTile(1, Colors.orange, 'SA'),
-                    buildTile(2, Colors.red, 'RE'),
-                    buildTile(3, Colors.green, 'GA'),
-                    buildTile(4, Colors.blue, 'MA'),
-                    buildTile(5, Colors.purple, 'PA'),
-                    buildTile(6, Colors.yellow, 'DHA'),
-                    buildTile(7, Colors.lightGreen, 'NI'),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildTile(int num, Color color, String note) {
-    return Expanded(
-      child: Column(
+      body: Column(
         children: [
-          Container(
-            color: color,
-            height: 300,
-            child: TextButton(
-                style: const ButtonStyle(
-                    foregroundColor: MaterialStatePropertyAll(Colors.white)),
-                onPressed: () {
-                  playTone(num);
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(note),
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      height: 30,
-                      width: 30,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.black),
-                    )
-                  ],
-                )),
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: ' Enter a new contact name...',
+            ),
           ),
+          TextButton(onPressed: (){
+            final contact = Contact(name: _controller.text);
+            ContactBook().add(contact: contact);
+            Navigator.of(context).pop();
+          }, child: const Text('Add contact'))
         ],
       ),
     );
   }
-}
-
-void playTone(int num) {
-  final audioplayer = AudioPlayer();
-  audioplayer.play(AssetSource('assets_note$num.wav'));
 }
